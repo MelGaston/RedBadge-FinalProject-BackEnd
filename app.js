c46272e6d1eb64d271374d38be540c057e09a0ba
Kellen Julian <kellenjulianweb@gmail.com>
2019-02-12 11:58:54 -0500

    commit to kellen branch

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+var express = require('express');
+var app = express();
+var db = require('./db');
+var user = require('./controllers/usercontroller');
+var card = require('./controllers/cardcontroller');
+var comment = require('./controllers/commentcontroller');
+var bodyParser = require('body-parser');
+
+app.use(require("./middleware/headers"));
+
+db.sync();
+app.use(bodyParser.json());
+app.use('/auth', user);
+app.use(require('./middleware/validate-session'))
+app.use('/card', post);
+app.listen(process.env.PORT, function(){
+    console.log(`App is listening on ${process.env.PORT}`);
+})
