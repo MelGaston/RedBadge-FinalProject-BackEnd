4760634a337218f9cf439abdac6c36497920ae0e
Kellen Julian <kellenjulianweb@gmail.com>
2019-02-25 16:46:57 -0500

    asdasda

--- models/cardmodel.js
+++ models/cardmodel.js
@@ -29,8 +29,7 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: false
         },
         notes: {
-            type: DataTypes.STRING,
-            allowNull: false
+            type: DataTypes.STRING
         },
         type: {
             type: DataTypes.STRING,
